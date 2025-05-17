673a22ac52e8758241cc1b19ac6826b62839fa62
Thamsanqa <thamzafaniso@gmail.com>
2025-05-17 20:14:36 +0200

    Fix: add ORDER BY clause to LEFT JOIN query

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -3,10 +3,14 @@ SELECT b.id AS booking_id, u.id AS user_id, u.name AS user_name
 FROM bookings b
 INNER JOIN users u ON b.user_id = u.id;
 
--- LEFT JOIN: Retrieve all properties with their reviews (including those with no reviews)
-SELECT p.id AS property_id, p.title AS property_title, r.id AS review_id, r.comment
-FROM properties p
-LEFT JOIN reviews r ON p.id = r.property_id;
+-- LEFT JOIN to retrieve all properties and their reviews
+SELECT properties.id AS property_id,
+       properties.name AS property_name,
+       reviews.id AS review_id,
+       reviews.comment
+FROM properties
+LEFT JOIN reviews ON properties.id = reviews.property_id
+ORDER BY properties.id;
 
 -- FULL OUTER JOIN: Retrieve all users and bookings, even if no match exists
 -- For MySQL or SQLite, use UNION of LEFT and RIGHT JOIN (since FULL OUTER JOIN is not supported)
