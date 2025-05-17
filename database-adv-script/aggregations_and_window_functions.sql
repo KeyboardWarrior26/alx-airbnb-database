2179eab1975429654168cc8f7baea516dc5a4c84
Thamsanqa <thamzafaniso@gmail.com>
2025-05-17 20:42:39 +0200

    Use RANK() to rank properties by booking count

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -5,10 +5,11 @@ LEFT JOIN bookings b ON u.id = b.user_id
 GROUP BY u.id, u.name
 ORDER BY total_bookings DESC;
 
--- 2. Rank properties by total number of bookings using ROW_NUMBER
+-- 2. Rank properties by total number of bookings using RANK
 SELECT p.id AS property_id, p.name, COUNT(b.id) AS booking_count,
-       ROW_NUMBER() OVER (ORDER BY COUNT(b.id) DESC) AS rank
+       RANK() OVER (ORDER BY COUNT(b.id) DESC) AS rank
 FROM properties p
 LEFT JOIN bookings b ON p.id = b.property_id
 GROUP BY p.id, p.name
 ORDER BY rank;
+
